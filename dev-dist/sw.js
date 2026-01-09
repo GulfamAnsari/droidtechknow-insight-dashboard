fbb611fd155c59c423be1940a0d3aed15a71035c
Gulfam <mohd.gulfam@walmart.com>
2026-01-09 20:55:24 +0530

    a

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vn9svoo2n1g"
+    "revision": "0.om4k7r24b8o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
